94902ead88679d5ee9c9ba57da23aefb1bb148f6
Doug Whitney <doug@metarouter.io>
2020-03-18 08:31:03 -0600

    Setup postcss import and break up styles

--- src/templates/page.js
+++ src/templates/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import SEO from '../components/seo'
-import Layout from '../components/layout'
+import Layout from '../components/layout/layout'
 
 export default ({ data, pageContext }) => {
   const { markdownRemark } = data
