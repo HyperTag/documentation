766f1f6b52415a4251f77d05be644d55ef9107ef
Doug Whitney <doug@metarouter.io>
2020-03-20 13:10:39 -0600

    Layout basic cards for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { StaticQuery, Link, graphql } from 'gatsby'
 
 import Layout from './../components/layout/layout'
 import SEO from './../components/seo'
@@ -21,6 +21,29 @@ export default () => (
           <SEO />
 
           <section>
+            <ul class="cards">
+              <li>
+                <Link to="/enterprise-edition/">MetaRouter Enterprise</Link>
+              </li>
+              <li>
+                <Link to="/enterprise-destinations/">
+                  Enterprise Destinations
+                </Link>
+              </li>
+              <li>
+                <Link to="/cloud-edition/">MetaRouter Cloud</Link>
+              </li>
+              <li>
+                <Link to="/cloud-destinations/">Cloud Destinations</Link>
+              </li>
+              <li>
+                <Link to="/sources-and-sdks/">Sources &amp; SDKs</Link>
+              </li>
+              <li>
+                <a href="https//metarouter.io/#contact">Help</a>
+              </li>
+            </ul>
+
             <div
               dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
             />
