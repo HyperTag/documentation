0d955f2c9f72d35144e932660d8620271d51b635
Doug Whitney <doug@metarouter.io>
2020-03-26 10:26:08 -0600

    Card styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,22 +20,39 @@ export default () => {
       <section>
         <ul className="cards">
           <li>
-            <Link to="/enterprise-edition/">MetaRouter Enterprise</Link>
+            <Link to="/enterprise-edition/">
+              <h1>MetaRouter Enterprise</h1>
+              <p>Deploy an end-to-end data router on your own cloud with maximum control, flexibility, and security.</p>
+            </Link>
           </li>
           <li>
-            <Link to="/cloud-edition/">MetaRouter Cloud</Link>
+            <Link to="/cloud-edition/">
+              <h1>MetaRouter Cloud</h1>
+              <p>Securely stream events from web, mobile, and server side sources using our cloud-based platform.</p>
+            </Link>
           </li>
           <li>
-            <Link to="/enterprise-destinations/">Enterprise Destinations</Link>
+            <Link to="/enterprise-destinations/">
+              <h1>Enterprise Destinations</h1>
+              <p></p>
+            </Link>
           </li>
           <li>
-            <Link to="/cloud-destinations/">Cloud Destinations</Link>
+            <Link to="/cloud-destinations/">
+              <h1>Cloud Destinations</h1>
+            </Link>
           </li>
           <li>
-            <Link to="/sources-and-sdks/">Sources &amp; SDKs</Link>
+            <Link to="/sources-and-sdks/">
+              <h1>Sources &amp; SDKs</h1>
+              <p>MetaRouter client-side and server-side integrations for your preferred development stack.</p>
+            </Link>
           </li>
           <li>
-            <a href="https://metarouter.io/#contact">Help</a>
+            <a href="https://metarouter.io/#contact">
+              <h1>Help</h1>
+              <p></p>
+            </a>
           </li>
         </ul>
 
