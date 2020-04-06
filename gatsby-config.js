78e3a95e6cfbb105e181490fcbadbffe10832834
Doug Whitney <doug@metarouter.io>
2020-04-06 15:26:06 -0600

    Enable offline access, upgrade deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,8 +46,6 @@ module.exports = {
         icon: 'src/images/logo-light.png',
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: ['./src/**/*.js'],
-  css: ['./src/**/*.css', './src/components/layout/code.css'],
+  css: ['./src/**/*.css'],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
