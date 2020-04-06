675711bd8b4578c0367f58dcff0fa892f29c42b6
Doug Whitney <doug@metarouter.io>
2020-04-06 14:48:38 -0600

    Optimize CSS output

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,17 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: ['./src/**/*.js'],
+  css: ['./src/**/*.css', './src/components/layout/code.css'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+})
+
 module.exports = () => ({
-  plugins: [require('postcss-easy-import'), require('tailwindcss')],
+  plugins: [
+    require('postcss-easy-import'),
+    require('tailwindcss'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 })
