3c26894f4f32afad44c2962149e63f7e5b57a755
Doug Whitney <doug@metarouter.io>
2020-04-09 09:13:00 -0600

    Fix for tailwind omitting classes in prod build

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: ['./src/**/*.js'],
   css: ['./src/**/*.css'],
+  whitelist: ['hidden', 'font-bold'],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
